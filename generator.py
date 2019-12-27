d0ee836a6fe21199007615835cfa2354a243ae3f
Ivan Ioshchenko <iioshchenko@griddynamics.com>
2019-12-27 08:06:11 -0500

    added path validation

--- generator.py
+++ generator.py
@@ -2,17 +2,18 @@ import argparse
 from validator import validate
 
 parser = argparse.ArgumentParser(description='Console utility for generating test data based on provided data schema')
-parser.add_argument('path_to_save_files', help='Where all files need to save')
-args = parser.parse_args()
+parser.add_argument('-path_to_save_files', help='Where all files need to save')
+
 
 
 def generate(args):
-    res = validate(args)
-    if res:
-        print(res)
+    result = validate(args)
+    if result:
+        print(f'Error: {result}')
     else:
         print('generate')
 
 
 if __name__ == '__main__':
+    args = parser.parse_args()
     generate(args)
